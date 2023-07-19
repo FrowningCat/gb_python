0087babd79633cf1f4c9d784df069d489c2b10ee
FrowningCat <vsavinkov60@gmail.com>
2023-07-19 17:43:40 +0300

    add_ex_4

---
+++ lesson_one/ex_4.py
@@ -0,0 +1,11 @@
+for i in range(2, 11):
+    for k in range(2, 6):
+        print(f'{k} * {i} = {i * k}\t', end='')
+    print('')
+
+print()
+
+for i in range(1, 10):
+    for k in range(6, 10):
+        print(f'{k} * {i} = {i * k}\t', end='')
+    print('')
