46819a041a5e0a9743ac3a937388271c4b1fea41
FrowningCat <vsavinkov60@gmail.com>
2023-07-26 19:08:37 +0300

    big fix ex_9 in lesson_three

--- lesson_three/ex_9.py
+++ lesson_three/ex_9.py
@@ -8,17 +8,15 @@ while i < 10:
     x = 0
     t = 0
     number = 0
-    # Находим самое часто повторяюшееся слово
     while x < len(text):
         if text.count(text[x]) > number:
             word = text[x]
             number = text.count(text[x])
         x += 1
-    # Заносим слово в масив
     list_words.append(word)
-    # Удоляем это слово из списка и проходим этот цикл еще 9 раз
     while t < len(text):
-        text.remove(word)
+        if word in text:
+            text.remove(word)
         t += 1
     i += 1
 
