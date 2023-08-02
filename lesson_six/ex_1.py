18156f03723e09a9c79a9f65ac63ee3a27c05a28
FrowningCat <vsavinkov60@gmail.com>
2023-08-02 11:26:20 +0300

    add ex_1 in lesson_6

---
+++ lesson_six/ex_1.py
@@ -0,0 +1,10 @@
+import os.path as op
+from sys import getsizeof as get_Siz
+import decimal as loooooon_nam
+import math as m
+
+str = "C:/Users/Admin/Desktop/Утилиты/Новая папкa/wow.png"
+*way, fulname = op.split(str)
+get_Siz(way)
+loooooon_nam.getcontext().prec = 42
+pi = m.pi
